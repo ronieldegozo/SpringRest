73ff72a2a6e3fe2c2bb2e0fb4844a4d828a23633
ronieldegozo <ronieldegozo@gmail.com>
2024-03-02 16:24:18 -0800

    Initial commit

---
+++ src/main/java/com/rougeniel/demo/rest/RestController.java
@@ -0,0 +1,16 @@
+package com.rougeniel.demo.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/test")
+public class RestController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+}
