8f7b016bc16f828741fbc9b73d08111625d69664
ronieldegozo <ronieldegozo@gmail.com>
2024-03-02 17:37:54 -0800

    Handle Error Handling

--- src/main/java/com/rougeniel/demo/controller/StudentController.java
+++ src/main/java/com/rougeniel/demo/controller/StudentController.java
@@ -1,11 +1,12 @@
 package com.rougeniel.demo.controller;
 
+import com.rougeniel.demo.model.ErrorResponse;
+import com.rougeniel.demo.model.NotFoundException;
 import com.rougeniel.demo.model.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +33,34 @@ public class StudentController {
     @GetMapping("/student/{studentId}")
     public Student getStudent(@PathVariable int studentId){
 
+        if(studentId >= students.size() || studentId <0 ){
+            throw new NotFoundException("Student ID not found : " + studentId);
+        }
+
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(NotFoundException ex){
+
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    //Handle all exception
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
 }
