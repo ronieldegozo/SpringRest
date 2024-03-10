70520f8b34dcdd4f9487d1ea3d9246576587b4e0
ronieldegozo <ronieldegozo@gmail.com>
2024-03-09 16:10:09 -0800

    Handle Error Handling Exception

--- src/main/java/com/rougeniel/demo/controller/StudentController.java
+++ src/main/java/com/rougeniel/demo/controller/StudentController.java
@@ -1,12 +1,12 @@
 package com.rougeniel.demo.controller;
 
-import com.rougeniel.demo.model.ErrorResponse;
 import com.rougeniel.demo.model.NotFoundException;
 import com.rougeniel.demo.model.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,29 +38,6 @@ public class StudentController {
         }
 
         return students.get(studentId);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(NotFoundException ex){
-
-        ErrorResponse error = new ErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    //Handle all exception
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(Exception ex){
-
-        ErrorResponse error = new ErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
 }
---
+++ src/main/java/com/rougeniel/demo/model/RestExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.rougeniel.demo.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(NotFoundException ex){
+
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    //Handle all exception
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+}
